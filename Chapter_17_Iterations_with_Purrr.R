9c9bb662f851b950d1ec64a5716c0325d6a4af43
SMemic4 <129231670+SMemic4@users.noreply.github.com>
2023-04-18 15:21:11 -0400

    Update Chapter_17_Iterations_with_Purrr.R

--- Chapter_17_Iterations_with_Purrr.R
+++ Chapter_17_Iterations_with_Purrr.R
@@ -118,7 +118,6 @@ df <-tibble(a = rnorm(10),
             d = rnorm(10))
 
 
-
 rescale01 <- function(x) {
   rng <- range(x, na.rm = TRUE)
   (x - rng[1]) / (rng[2] - rng[1])
@@ -322,4 +321,152 @@ col_summary2() <- function(df, fun){
 #################################################################################################################################################################################
 # Map Functions
 #################################################################################################################################################################################
+# The purrr package provides a family of functions for looping over vectors and doing something to each element. There are several functions for each type of output
+# map() makes a list 
+# map_lgl() makes a logical vector
+# map_int() makes an integer vector
+# map_dbl() makes a double vector
+# map_chr() makes a character vector
+# Each of these functions takes a vector as an input and applies a function to each piece and then returns a new vector with the same length (and names) as the input
+# The type of vector is determined by the suffix to the map function
+# The main advantage of using a map function is not speed but clrity 
+
+
+df <-tibble(a = rnorm(10),
+            b = rnorm(10),
+            c = rnorm(10),
+            d = rnorm(10))
+
+map_dbl(df, mean) # These functions performs the same computation as the previous for loop
+map_dbl(df, median)
+map_dbl(df, sd)
+
+# Compared to for loops the purpose of the map functions is focused on the operation being performed and not the iteration of looping over each element
+
+df %>% map_dbl(mean)
+df %>% map_dbl(median)
+df %>% map_dbl(sd)
+
+# There are few key differences between map functions and col_summary()
+# 1. All purrr functions are implemented in C making them faster at the expense of readability
+# 2. The second argument in map functions (.f), the function to apply, can be a formula, a character vector, or an integer vector
+# 3. Map functions used (...) to pass along additional arguments to .f each time it's called
+
+map_dbl(df, mean, trim = 0.5)
+
+# 4. Map functions also preserve names
+
+# Imagine creating a linear fut model for each group within a dataset. The following example splits up mtcars dataset into three pieces (one for each type of cylinder) amd fits the same linear model to each piece
+
+models <- mtcars %>% split(.$cyl) %>% map(function(df) lm(mpg ~ wt, data = df))
+models
+
+# Purrr provides a convenient short-cut - a one-sided formula:
+
+models <- mtcars %>% split(.$cyl) %>% map(~lm(mpg ~ wt, data = .))
+models
+
+# The . here is used as a pronoun. It refers to the current list element (in the same way that i referred to the current index in the for loop)
+
+# When observing many models, to extract a summary statistics such as R^2 it is necessary to run a summary() function and hen extract the r^2 component 
+
+models %>% map(summary) %>% map_dbl(~.$r.squared)
+
+# Purr has an additional shortcut which allows for common operations to be called for using a string
+
+models %>% map(summary) %>% map_dbl("r.squared")
+
+# An integer can be used to select elements by position:
+
+x <- list(list(1, 2, 3), list(4, 5, 6), list(7, 8, 9))
+x %>% map_dbl(2) # 2, 5, 8
+
+# the apply() family of functions share similarities with the purrr functions
+
+# lapply() is basically identical to map(), expect that map() is consistent with all the other functions in purrr, and shortcuts can be used for .f (functions)
+# Base sapply() is a wrapper around lappy() that automatically simplifies the output. 
+# vapply() is a safer alternative to sapply() due to it only requiring the additional argument that defines the type. 
+
+#################################################################################################################################################################################
+# Exercises
+#################################################################################################################################################################################
+# 1. Write code that uses one of the map functions to:
+
+# a. Compute the mean of every column in mtcars
+
+mtcars %>% map_dbl(mean)
+
+# b. Determines the type of each column in nycflights13::flights
+
+flights %>% map_chr(typeof)
+
+# c. Compute the number of unique values in each column of iris.
+
+iris %>% map_int(n_distinct)
+
+# d. To generate 10 random normals for each of μ = -10, 0, 10, and 100. The result is a list of numeric vectors. 
+
+map(c(-10, 0, 10, 100), ~rnorm(n = 10, mean = .))
+
+# 2. How can you create a single vector that for each column in a data frame indicates whether or not it’s a factor?
+
+diamonds %>% map_lgl(is.factor)
+
+# 3. What happens when you use the map functions on vectors that aren’t lists? What does map(1:5, runif) do? Why?
+
+# Map functions can be used on any vector or list however it will always return a list no matter the output. Secondly, when running map(1:5, runif) it will run through that list 5 times selecting and drawing from additional numbers as it progresses up the list.
+
+# 4. What does map(-2:2, rnorm, n = 5) do? Why? What does map_dbl(-2:2, rnorm, n = 5) do? Why?
+
+map(-2:2, rnorm, n = 5) 
+map_dbl(-2:2, rnorm, n = 5) 
+
+# The first expression takes samples from a group of five with normal distributions with means of -2, -1, 0, 1, and 2 and returns a list of numeric vectors with a length of 5
+# The second expression falls because the map function requires each element it applies to you to return a vector a 1. 
+
+#################################################################################################################################################################################
+# Dealing with Failure
+#################################################################################################################################################################################
+# The more map functions that used for operations the higher the chance of failure occurring 
+# When this occurs, an error message will appear and no output will be received 
+# safely() is a function that takes a function and returns a modified version of it. 
+# Safely() will never throw an error, instead it always returns a list with two elements:
+# 1. Result - The original result. If there was an error, this will be NULL
+# 2. Error - An error object, If the operation was successful this will be NULL
+# These are similar to the try() function in base R
+
+safe_log <- safely(log)
+str(safe_log(10)) # $ result: num 2.3,  $ error : NULL
+
+str(safe_log("a")) # $ result: NULL , $ message: chr "non-numeric argument to mathematical ..."
+
+# When the function succeeds the result element contains the result and the error element is NULL
+# When the function fails, the result element is NULL and the error element contains an error object
+
+# safely() is designed to work with map:
+
+x <- list(1, 10, "a")
+y <- x %>% map(safely(log))
+str(y)
+
+# transpose() can be used to create two lists, one with all of the outputs and one with all of the errors. It is a purrr function
+
+y <- y %>% transpose()
+str(y)
+
+# purr provides two other useful functions:
+# 1. possibly() always succeeds. It's simpler than safely() because it returns a default value when there is an error
+
+x <- list(1, 10, "a")
+x %>% map_dbl(possibly(log, NA_real_)) # 0.000000 2.302585  NA
+
+# 2. quietly() performs a similar role to safely(), but instead of capturing errors, it captures printed output, messages, and warnings: 
+
+x <- list(1, -1)
+x %>% map(quietly(log)) %>% str()
+
+#################################################################################################################################################################################
+# Mapping over Multiple Arguments
+#################################################################################################################################################################################
+
 
