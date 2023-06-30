3cecd0f47f70e3c2c18eb71c3024435ea83d3e57
SMemic4 <129231670+SMemic4@users.noreply.github.com>
2023-06-30 14:28:14 -0400

    Add files via upload

---
+++ RDS_Chapter_8_Managing_Data_Frames_With_The_Dplyr_Package.R
@@ -0,0 +1,28 @@
+####################################################################################################################################################################
+# Chapter 7: Managing Data Frames with the dplyr package
+####################################################################################################################################################################
+# Data Frames
+####################################################################################################################################################################
+# The data frame is a key structure in statistics and in R
+# The basic structure of a data frame is that there is one observation per row and each column represents a variable, a measurement, feature, or characteristic of the observation
+# The dplyr package is desgined to mitigate a lot of problems when it comes to filtering, ordering, and extracting from data frames
+
+####################################################################################################################################################################
+# The dplyr Package
+####################################################################################################################################################################
+# One important contribution of the dplyr package is that it provides a "grammar" for data manipulation and for operating on data frames
+
+####################################################################################################################################################################
+# dplyr Grammar 
+####################################################################################################################################################################
+# Some of the key verbs provided by the dplyr package
+# select: return a subset of the columns of a data frame, using a flesxible notation
+# filter: extract a subset of rows from a data frame based on logical conditions
+# arrange: reorder rows of a data frame
+# rename: rename variables in a data frame
+# mutate: add new variable/columns or transform existing variables 
+# summarize: generate summary statistics of different variables in the data frame, possibly within strata
+
+
+
+
